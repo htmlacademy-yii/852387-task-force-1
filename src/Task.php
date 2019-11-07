413bbdedddbfd390029a42a157599198c0f2b7d0
tankov <tankov@192.168.0.11>
2019-11-07 22:06:11 +0300

    add test for class Task

--- src/Task.php
+++ src/Task.php
@@ -15,19 +15,19 @@ class Task
     const ACTION_EXECUTE = 'execute';
     const ACTION_REPLY = 'reply';
 
-    const ACTIONS_MAP = [
-        self::STATUS_NEW => [self::ACTION_CANCEL, self::ACTION_REPLY],
+    const STATUS_MAP = [
+        self::STATUS_NEW => [self::STATUS_CANCEL, self::STATUS_ACTIVE],
         self::STATUS_CANCEL => [],
         self::STATUS_DONE => [],
         self::STATUS_FAIL => [],
-        self::STATUS_ACTIVE => [self::ACTION_REFUSE, self::ACTION_EXECUTE]
+        self::STATUS_ACTIVE => [self::STATUS_DONE, self::STATUS_FAIL]
     ];
 
-    const STATUS_MAP = [
-        self::ACTION_CANCEL => [self::STATUS_CANCEL],
-        self::ACTION_REFUSE => [self::STATUS_FAIL],
-        self::ACTION_EXECUTE => [self::STATUS_DONE],
-        self::ACTION_REPLY => [self::STATUS_ACTIVE]
+    const ACTION_TO_STATUS = [
+        self::ACTION_CANCEL => self::STATUS_CANCEL,
+        self::ACTION_REFUSE => self::STATUS_FAIL,
+        self::ACTION_EXECUTE => self::STATUS_DONE,
+        self::ACTION_REPLY => self::STATUS_ACTIVE
     ];
 
     /**
@@ -36,27 +36,11 @@ class Task
      *
      * @param string $action действие
      *
-     * @return array
+     * @return string
      */
-    public function getNextStatus(string $action) : array
+    public function getNextStatus(string $action)
     {
-        foreach (self::ACTIONS_MAP as $status) {
-            if (in_array($action, $status)) {
-                return $status;
-            }
-        } return null;
+        return in_array($action, self::ACTION_TO_STATUS) ? self::ACTION_TO_STATUS[$action] : null;
     }
 
-    /**
-     * Метод для возврата текущего статуса задачи для указанного действия
-     * getCurrentStatus
-     *
-     * @param string $action действие
-     *
-     * @return void
-     */
-    public function getCurrentStatus(string $action)
-    {
-        return self::STATUS_MAP[$action];
-    }
 }
