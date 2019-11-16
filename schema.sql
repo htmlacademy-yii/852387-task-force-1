b123709ed8b8e5fd06dedfcfa82709875757e7c3
tankov <tankov@192.168.0.11>
2019-11-16 21:04:06 +0300

    fixed DATABASE

--- schema.sql
+++ schema.sql
@@ -1,109 +1,106 @@
+DROP DATABASE taskForce;
+
 CREATE DATABASE taskForce
   DEFAULT CHARACTER SET utf8
   DEFAULT COLLATE utf8_general_ci;
 
 USE taskForce;
 
-CREATE TABLE `users` {
-  `id` INT unsigned NOT NULL AUTO_INCTEMENT,
-  `name` CHAR(255) NOT NULL,
-  `email` CHAR(255) NOT NULL,
+CREATE TABLE `user` (
+  `id` INT unsigned NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(255) NOT NULL DEFAULT '',
+  `email` VARCHAR(255) NOT NULL DEFAULT '',
   `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  `password` CHAR(64) NOT NULL,
+  `password` VARCHAR(64) NOT NULL DEFAULT '',
   `birthday` DATE,
-  `city` CHAR(255),
-  `role` CHAR(255) NOT NULL, -- customer/worker
-  `info` CHAR,
-  `phone` INT,
-  `skype` CHAR(255),
-  `another_chat` CHAR(255),
-  `status_user` INT, -- занят над задачей/свободен(1/0)
+  `city` VARCHAR(255),
+  `role` INT NOT NULL, -- 1 === customer/ 2 === worker
+  `info` TEXT,
+  `phone` VARCHAR(255),
+  `skype` VARCHAR(255),
+  `another_chat` VARCHAR(255),
+  `status_user` INT, -- занят над задачей/свободен(1/2)
   `rating` INT,
-  `avatar` CHAR(255),
-  `last_activity_at` TIMESTAMP,
-  `popular` INT, -- сколько раз страница пользователя просматривалась
+  `avatar_photo` VARCHAR(255),
+  `last_activity_at` DATETIME,
+  `page_visit_count` INT, -- сколько раз страница пользователя просматривалась
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`)
-} ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE `favorites` {
-  `user_customer_id` INT unsigned NOT NULL,
-  `user_worker_id` INT unsigned NOT NULL
-} ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE `favorit` (
+  `customer_id` INT unsigned NOT NULL,
+  `worker_id` INT unsigned NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE `specializations` {
+CREATE TABLE `user_category` (
   `user_id` INT unsigned NOT NULL,
-  `courier` TINYINT DEFAULT '0',
-  `remote` TINYINT DEFAULT '0',
-  `repair_transport` TINYINT DEFAULT '0',
-  `trasportation` TINYINT DEFAULT '0',
-  `translation` TINYINT DEFAULT '0',
-  `visit` TINYINT DEFAULT '0'
-} ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `category` INT                       -- 10/20/30/..
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE `notifications` {
+CREATE TABLE `notification` (
   `user_id` INT unsigned NOT NULL,
-  `new_message` TINYINT DEFAULT '0',
-  `action_task` TINYINT DEFAULT '0',
-  `new_review` TINYINT DEFAULT '0',
-  `show_contacts` TINYINT DEFAULT '0',
-} ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `notice` INT                         -- 10/20/30/..
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE `tasks` {
+CREATE TABLE `task` (
   `id` INT unsigned NOT NULL AUTO_INCREMENT,
-  `user_customer_id` INT unsigned NOT NULL,
-  `user_worker_id` INT unsigned NOT NULL,
-  `category` CHAR(255),
-  `name_task` CHAR(255),
-  `icon_task` CHAR(255),
+  `customer_id` INT unsigned NOT NULL,
+  `worker_id` INT unsigned NOT NULL,
+  `category` INT,
+  `name_task` VARCHAR(255),
+  `icon_task` VARCHAR(255),
+  `task_status` INT,
   `add_date` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   `complite_date` DATE DEFAULT NULL,
-  `info_task` CHAR(255),
-  `file_url` CHAR(255), -- если несколько файлов, то нужна ещё таблица для файлов?
-  `location` CHAR(255),
+  `info_task` VARCHAR(255),
+  `file_url` VARCHAR(255), -- если несколько файлов, то нужна ещё таблица для файлов?
+  `is_remote` VARCHAR(5), -- yes/no
+  `location` VARCHAR(255),
   `price` INT,
-  `coordinates` CHAR(255),
-  PRIMARY KEY (`id`),
-} ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `coordinate` VARCHAR(255),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE `reviews` {
+CREATE TABLE `review` (
   `id` INT unsigned NOT NULL AUTO_INCREMENT,
   `user_id` INT unsigned NOT NULL,
-  `task_id` unsigned NOT NULL,
-  `text_review` CHAR(255),
-  `score` INT unsigned NOT NULL
-} ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-CREATE TABLE `task_statuses` {
   `task_id` INT unsigned NOT NULL,
-  `new` TINYINT DEFAULT '1',
-  `cancel` TINYINT DEFAULT '0',
-  `done` TINYINT DEFAULT '0',
-  `fail` TINYINT DEFAULT '0',
-  `active` TINYINT DEFAULT '0'
-} ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `text_review` VARCHAR(255),
+  `score` INT unsigned NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE `photos` {
+CREATE TABLE `photo` (
   `id` INT unsigned NOT NULL AUTO_INCREMENT,
   `task_id` INT unsigned NOT NULL,
   `user_id` INT unsigned NOT NULL,
-  `photo` CHAR(255)
-} ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `photo` VARCHAR(255),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE `comments` {
+CREATE TABLE `comment` (
   `id` INT unsigned NOT NULL AUTO_INCREMENT,
   `user_id` INT unsigned NOT NULL,
   `task_id` INT unsigned NOT NULL,
   `add_date` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  `role_user` CHAR(255),             -- customer/worker
-  `text_comment` CHAR(255)
-} ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `text_comment` VARCHAR(255),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE `responses` {
+CREATE TABLE `response` (
   `id` INT unsigned NOT NULL AUTO_INCREMENT,
   `user_id` INT unsigned NOT NULL,
   `task_id` INT unsigned NOT NULL,
   `price` INT,
-  `comment` CHAR(255)
-} ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `comment` VARCHAR(255),
+  `created_at` DATETIME,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+CREATE TABLE `attachment` (
+  `id` INT unsigned NOT NULL AUTO_INCREMENT,
+  `task_id` INT unsigned NOT NULL,
+  `filename` VARCHAR(225),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
